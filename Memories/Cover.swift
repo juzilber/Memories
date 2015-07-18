d8617ef5fb57dc9f9612594e7b373fde443ab75e
juliana dana <juliana_zilber@hotmail.com>
2015-07-18 16:38:41 -0300

    inicialização das classes

---
+++ Memories/Cover.swift
@@ -0,0 +1,19 @@
+//
+//  Cover.swift
+//  Memories
+//
+//  Created by Juliana Zilberberg on 7/18/15.
+//  Copyright (c) 2015 Juliana Zilberberg. All rights reserved.
+//
+
+import UIKit
+
+class Cover: NSObject{
+    
+    var photo : String = ""
+    var photo2 : String = ""
+    var name : String = ""
+    var title : String = ""
+
+}
+
---
+++ Memories/Moment.swift
@@ -0,0 +1,19 @@
+//
+//  Moment.swift
+//  Memories
+//
+//  Created by Juliana Zilberberg on 7/18/15.
+//  Copyright (c) 2015 Juliana Zilberberg. All rights reserved.
+//
+
+import UIKit
+
+class Moment : NSObject{
+    
+    var id:Int?
+    var photo:String = ""
+    var subtitle:String = ""
+    var audio:String = ""
+    
+    
+}
