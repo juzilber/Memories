6f4f4e5e4b3549f093810131e1709f888e6c8e8e
thiagogallegoc <thiagogallegoc@gmail.com>
2015-07-20 14:33:13 -0300

    xib funcionando
    Storyboard deletado
    Xib implementado com base em tutorial
    https://www.weheartswift.com/remove-storyboard-from-project/

--- Memories/AppDelegate.swift
+++ Memories/AppDelegate.swift
@@ -16,6 +16,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        
+        window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        
+        var registerCoverVC: RegisterCoverVC = RegisterCoverVC(nibName:"RegisterCoverVC", bundle: nil)
+        window?.rootViewController = registerCoverVC
+        
+        window?.makeKeyAndVisible()
+        
         return true
     }
 
