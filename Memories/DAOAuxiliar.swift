0daabb995023197791f0e6b207539a16b1cd22e1
juliana dana <juliana_zilber@hotmail.com>
2015-07-28 16:41:47 -0300

    commit DAOFamily

--- Memories/DAOAuxiliar.swift
+++ Memories/DAOAuxiliar.swift
@@ -9,14 +9,37 @@
 import Foundation
 
 
-
-
-
 class DAOAuxiliar {
+
     
-    
-    
-    
+    class func checkFiles(filename: String, type: String){
+        
+        var contents : NSMutableDictionary!;
+        
+        let familyPath : String;
+        
+        let familyPathDoc : String;
+
+        
+        var documentPath : String = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0] as! String;
+        var imgPath:String = documentPath
+        familyPathDoc = documentPath.stringByAppendingPathComponent("Family")
+        
+        familyPath = documentPath.stringByAppendingPathComponent("Family/FamilyData.plist");
+        
+        println(familyPath)
+        var fileManager:NSFileManager = NSFileManager.defaultManager();
+        
+        if(!fileManager.fileExistsAtPath(documentPath)){
+            let sourcePath:String = NSBundle.mainBundle().pathForResource(filename, ofType: type)!;
+            fileManager.copyItemAtPath(sourcePath, toPath: documentPath, error: nil);
+        }
+        
+        
+        var imgPessPath = imgPath.stringByAppendingPathComponent("imgProf");
+        
+        fileManager.createDirectoryAtPath(imgPessPath, withIntermediateDirectories:false, attributes:nil, error:nil);
+    }
     
     func getFreeIdInDict(dict:NSDictionary) -> String{
         
@@ -35,7 +58,6 @@ class DAOAuxiliar {
         return String(id);
         
     }
-    
     
     
     func loadPList() -> NSMutableDictionary?{
